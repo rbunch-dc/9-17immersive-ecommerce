30a16f2faec2f4d627b2faa715b7cd431f063a95
Robert Bunch <rob@digitalcrafts.com>
2017-12-11 14:34:25 -0500

    Update slick css and add corresponding module

--- front-end/src/components/SlickSlider.js
+++ front-end/src/components/SlickSlider.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 class SlickSlider extends Component{
 	render(){
