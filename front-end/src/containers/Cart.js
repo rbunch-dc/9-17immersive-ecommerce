3635ed2eb31a6936efda893702799ea7e0d85034
Robert Bunch <rob@digitalcrafts.com>
2017-12-18 12:36:05 -0500

    return to render the productName as we map through it

--- front-end/src/containers/Cart.js
+++ front-end/src/containers/Cart.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import GetCart from '../actions/GetCart';
+import { Link } from 'react-router-dom';
 
 class Cart extends Component{
 
@@ -18,9 +19,26 @@ class Cart extends Component{
 
 	render(){
 		console.log(this.props.cart);
-		return(
-			<h1>Cart Page</h1>
-		)
+		if(!this.props.cart.totalItems){
+			// if this return occurs, the render is DONE
+			return(
+				<div>
+					<h3>Your cart is empty! Get shopping or <Link to="/login">login</Link></h3>
+				</div>
+			)
+		}else{
+			var cartArray = this.props.cart.products.map((product,index)=>{
+				console.log(product)
+				return (
+					<h1>{product.productName}</h1>
+				)
+			})
+			return(
+				<div>
+					{cartArray}
+				</div>
+			)
+		}
 	}
 }
 
